7e77efab517b5e44a3f9f15e07137019776ad5df
quocsi014 <quocsi014@gmail.com>
2024-08-07 23:44:27 +0700

    create account entity

---
+++ server/entity/account.go
@@ -0,0 +1,20 @@
+package entity
+
+
+type Account struct{
+	Id string `json:"id" gorm:"column:id"`
+	Email string `json:"email" gorm:"column:email"`
+	Password string `json:"password" gorm:"column:password"`
+}
+
+func (a *Account)NewAccount(email, password string) *Account{
+	return &Account{
+		Email: email,
+		Password: password,
+	}
+}
+
+func (a *Account)TableName() string{
+	return "accounts"
+}
+
