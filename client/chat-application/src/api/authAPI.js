4dba8ae3c0ab74b179f680175afc246b74b775d7
quocsi014 <quocsi014@gmail.com>
2024-08-26 14:16:34 +0700

    register api

--- client/chat-application/src/api/authAPI.js
+++ client/chat-application/src/api/authAPI.js
@@ -1,11 +1,20 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL = import.meta.env.REACT_APP_API_URL
+const API_URL = import.meta.env.REACT_APP_API_URL;
 
-export const API_Login = (account, password)=>{
+export const API_Login = (account, password) => {
   let data = {
-    "account": account,
-    "password": password
-  }
-  return axios.post(`${API_URL}/auth/login`, data)
-}
+    account: account,
+    password: password,
+  };
+  return axios.post(`${API_URL}/auth/login`, data);
+};
+
+export const API_Register = (email, username, password) => {
+  let data = {
+    email: email,
+    username: username,
+    password: password,
+  };
+  return axios.post(`${API_URL}/auth/register`, data);
+};
